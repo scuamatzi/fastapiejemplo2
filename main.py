b7f2657c31684b82f3b70926596a7b9f2aa62649
Salvador Cuamatzi <escuamatzi@gmail.com>
2023-06-20 18:50:42 -0600

    I just cleaned the code deleting useless comments.

--- main.py
+++ main.py
@@ -17,31 +17,15 @@ datos={
 
 @app.get("/")
 async def inicio(request: Request):
-    #sin_codificar=json.dumps(datos)
-    #return json.loads(sin_codificar)
-    #print(len(datos))
-    #return json.loads(json.dumps(datos))
     result=json.loads(json.dumps(datos))
     return templates.TemplateResponse("inicio.html",{"request":request, "listado":result})
 
 @app.post("/add")
 async def add(request: Request):
-    #nuevos_datos={}
     formdata=await request.form()
-    #i=1
-
-    #for id in datos:
-    #    nuevos_datos[str(id)]=datos[id]
-    #    i+=1
-    
-    #datos[i]=formdata["nuevolenguaje"]
-    #print(datos["1"])
-    #print(len(datos))
     indexes=datos.keys()
     indexes_as_integers=[int(x) for x in indexes]
     new_index=max(indexes_as_integers)+1
-    print(new_index)
-    #datos[str(len(datos)+1)]=formdata["nuevolenguaje"]
     datos[str(new_index)]=formdata["nuevolenguaje"]
     return RedirectResponse("/",status_code=303)
 
